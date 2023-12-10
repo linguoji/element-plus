8d6d1af1d0734b43659775c98b0b310c9f7a4c07
linguoji <46948680+linguoji@users.noreply.github.com>
2023-12-10 10:46:15 +0800

    Update file-list-with-thumbnail.vue

--- docs/examples/upload/file-list-with-thumbnail.vue
+++ docs/examples/upload/file-list-with-thumbnail.vue
@@ -3,7 +3,6 @@
     v-model:file-list="fileList"
     class="upload-demo"
     action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-    :on-preview="handlePreview"
     :on-remove="handleRemove"
     list-type="picture"
   >
